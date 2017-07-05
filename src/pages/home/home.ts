475faff138ec12139cd1c345b09259060510632c
alfcastillo90 <cacr1990@gmail.com>
2017-07-05 02:13:53 -0400

    Se agregaron dos formularios de registro con sus respectivos FormBuilder

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    @ViewChild('signupSlider') signupSlider : any;
+    slideOneForm: FormGroup;
+    slideTwoForm: FormGroup;
+    submitAttempt: boolean = false;
+    
+    constructor(public navCtrl: NavController, public formBuilder: FormBuilder) {
+        this.slideOneForm = formBuilder.group({
+            firstName : [''],
+            lastName : [''],
+            age:['']
+        });
 
-  }
+        this.slideTwoForm = formBuilder.group({
+            username : [''],
+            password : [''],
+            bio:['']
+        });
+    }
+    
+    next(){
+        this.signupSlider.slideNext();
+    }
+    
+    prev(){
+        this.signupSlider.slidePrev();
+    }
+    
+    save(){
+        
+    }
 
 }
